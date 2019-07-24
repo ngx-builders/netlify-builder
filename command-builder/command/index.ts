52e8c707ef4f88415d38306419ecb0b43e6e7ef8
Santosh Yadav <santosh.yadav198613@gmail.com>
2019-07-24 13:56:55 +0530

    added code to deploy

--- command-builder/command/index.ts
+++ command-builder/command/index.ts
@@ -1,8 +1,12 @@
 import { BuilderContext, BuilderOutput, createBuilder } from '@angular-devkit/architect';
 import { JsonObject } from '@angular-devkit/core';
+const NetlifyAPI = require('netlify')
 
 interface Options extends JsonObject {
   command: string;
+  outputPath: string;
+  netlifyToken: string,
+  siteId: string,
   args: string[];
 }
 
@@ -10,5 +14,16 @@ export default createBuilder<Options>(
   async (builderConfig: any, context: BuilderContext): Promise<BuilderOutput> => {
     context.reportStatus(`Executing "${builderConfig.command}"...`);
     console.log(builderConfig.command);
+    console.log(builderConfig.outputPath);
+    const client: any = new NetlifyAPI(builderConfig.netlifyToken,
+      {
+        userAgent: 'netlify/js-client',
+        scheme: 'https',
+        host: 'api.netlify.com',
+        pathPrefix: '/api/v1',
+        globalParams: {}
+      });
+
+    const response = await client.deploy(builderConfig.siteId, builderConfig.outputPath);
     return { success: true };
   });
